bc906ce9067f577437e38b1e85e8e10f24e6a079
Guillermo Valdés <guillermo@movimientolibre.com>
2021-02-11 15:08:58 -0600

    Abogados resgitrados con año desde y hasta.

--- api/abogados/views.py
+++ api/abogados/views.py
@@ -12,10 +12,23 @@ router = APIRouter()
 
 
 @router.get("")
-async def abogados_activos(ano: Optional[int] = None, fecha: Optional[date] = None, libro: Optional[str] = None, nombre: Optional[str] = None):
+async def abogados_activos(
+    ano: Optional[int] = None,
+    ano_desde: Optional[int] = None,
+    ano_hasta: Optional[int] = None,
+    fecha: Optional[date] = None,
+    libro: Optional[str] = None,
+    nombre: Optional[str] = None,
+):
     """ Lista de Abogados activos """
     consulta = db.session.query(Abogado)
-    if ano:
+    if ano_desde:
+        desde = datetime.strptime(f"{ano_desde}-01-01", "%Y-%m-%d")
+        consulta = consulta.filter(Abogado.fecha >= desde)
+    if ano_hasta:
+        hasta = datetime.strptime(f"{ano_hasta}-12-31", "%Y-%m-%d")
+        consulta = consulta.filter(Abogado.fecha <= hasta)
+    if ano and not (ano_desde or ano_hasta):
         desde = datetime.strptime(f"{ano}-01-01", "%Y-%m-%d")
         hasta = datetime.strptime(f"{ano}-12-31", "%Y-%m-%d")
         consulta = consulta.filter(Abogado.fecha >= desde).filter(Abogado.fecha <= hasta)
