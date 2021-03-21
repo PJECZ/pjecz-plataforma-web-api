b8105cf5c131ccbf566e78d0477cbe5e5bbb590e
Guillermo Valdés <guillermo@movimientolibre.com>
2021-03-21 17:57:40 -0600

    Agregué config/settings.py

---
+++ config/settings.py
@@ -0,0 +1,22 @@
+"""
+Configuración para producción
+"""
+import os
+
+
+# MySQL en Google Cloud
+DB_USER = os.environ.get("DB_USER", "nouser")
+DB_PASS = os.environ.get("DB_PASS", "wrongpassword")
+DB_NAME = os.environ.get("DB_NAME", "pjecz_plataforma_web")
+DB_SOCKET_DIR = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+CLOUD_SQL_CONNECTION_NAME = os.environ.get("CLOUD_SQL_CONNECTION_NAME", "none")
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket={DB_SOCKET_DIR}/{CLOUD_SQL_CONNECTION_NAME}"
+
+# CORS or "Cross-Origin Resource Sharing" refers to the situations when a frontend
+# running in a browser has JavaScript code that communicates with a backend,
+# and the backend is in a different "origin" than the frontend.
+# https://fastapi.tiangolo.com/tutorial/cors/
+ORIGINS = [
+    "https://www.pjecz.gob.mx",
+    "https://www.poderjudicialcoahuila.gob.mx",
+]
