95c8204bb4df598eea80a513261f61607f6543de
Guillermo Valdés <guillermo@movimientolibre.com>
2021-04-17 12:48:13 -0500

    Necesita esta constante en settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ DB_SOCKET_DIR = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
 CLOUD_SQL_CONNECTION_NAME = os.environ.get("CLOUD_SQL_CONNECTION_NAME", "none")
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket={DB_SOCKET_DIR}/{CLOUD_SQL_CONNECTION_NAME}"
 
+# Always in False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # CORS or "Cross-Origin Resource Sharing" refers to the situations when a frontend
 # running in a browser has JavaScript code that communicates with a backend,
 # and the backend is in a different "origin" than the frontend.
