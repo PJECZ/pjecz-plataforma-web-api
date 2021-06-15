0b4bb9d019cb167df5626454fb960a9d02504023
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-14 22:26:00 -0500

    Inicié Materias.

--- api/autoridades/models.py
+++ api/autoridades/models.py
@@ -1,7 +1,8 @@
 """
 Autoridades, modelos
 """
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from collections import OrderedDict
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from lib.database import Base
 from lib.universal_mixin import UniversalMixin
@@ -9,6 +10,16 @@ from lib.universal_mixin import UniversalMixin
 
 class Autoridad(Base, UniversalMixin):
     """Autoridad"""
+
+    ORGANOS_JURISDICCIONALES = OrderedDict(
+        [
+            ("NO DEFINIDO", "No Definido"),
+            ("JUZGADO DE PRIMERA INSTANCIA", "Juzgado de Primera Instancia"),
+            ("PLENO O SALA DEL TSJ", "Pleno o Sala del TSJ"),
+            ("TRIBUNAL DISTRITAL", "Tribunal Distrital"),
+            ("TRIBUNAL DE CONCILIACION Y ARBITRAJE", "Tribunal de Conciliación y Arbitraje"),
+        ]
+    )
 
     # Nombre de la tabla
     __tablename__ = "autoridades"
@@ -19,12 +30,19 @@ class Autoridad(Base, UniversalMixin):
     # Clave foránea
     distrito_id = Column(Integer, ForeignKey("distritos.id"), index=True, nullable=False)
     distrito = relationship("Distrito", back_populates="autoridades")
+    materia_id = Column(Integer, ForeignKey("materias.id"), index=True, nullable=False)
+    materia = relationship("Materia", back_populates="autoridades")
 
     # Columnas
     descripcion = Column(String(256), nullable=False)
     clave = Column(String(16), nullable=False, unique=True)
     es_jurisdiccional = Column(Boolean(), nullable=False, default=False)
     es_notaria = Column(Boolean(), nullable=False, default=False)
+    organo_jurisdiccional = Column(
+        Enum(*ORGANOS_JURISDICCIONALES, name="tipos_organos_jurisdiccionales", native_enum=False),
+        index=True,
+        nullable=False,
+    )
     directorio_edictos = Column(String(256))
     directorio_glosas = Column(String(256))
     directorio_listas_de_acuerdos = Column(String(256))
--- sql/autoridades.session.sql
+++ sql/autoridades.session.sql
@@ -1,5 +1,4 @@
 -- @block listar
--- @conn pjecz_plataforma_web
 SELECT autoridades.id,
     distritos.nombre,
     autoridades.descripcion
@@ -8,3 +7,18 @@ FROM distritos
 WHERE autoridades.estatus = 'A'
 ORDER BY distritos.nombre,
     autoridades.descripcion;
+-- @block autoridades y materias
+SELECT distritos.nombre AS distrito,
+    autoridades.clave,
+    autoridades.descripcion
+FROM autoridades
+    INNER JOIN distritos ON autoridades.distrito_id = distritos.id
+WHERE distritos.es_distrito_judicial = True
+    AND autoridades.es_jurisdiccional = True
+    AND autoridades.es_notaria = False
+    AND autoridades.estatus = 'A'
+ORDER BY distrito,
+    clave ASC -- @block materias
+SELECT nombre
+FROM materias
+ORDER BY id ASC
