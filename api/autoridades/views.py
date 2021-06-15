35643d99d28d9955a69345938f69328b5c142f0a
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-15 15:58:28 -0500

    Corrijo obtener una autoridad.

--- api/autoridades/views.py
+++ api/autoridades/views.py
@@ -13,7 +13,7 @@ router = APIRouter()
 
 @router.get("", response_model=List[schemas.Autoridad])
 async def listar_autoridades(distrito_id: int = None, materia_id: int = None, organo_jurisdiccional: str = None, con_notarias: bool = False, db: Session = Depends(get_db)):
-    """ Lista de Autoridades """
+    """Lista de Autoridades"""
     resultados = []
     for autoridad, distrito, materia in crud.get_autoridades(db, distrito_id=distrito_id, materia_id=materia_id, organo_jurisdiccional=organo_jurisdiccional, con_notarias=con_notarias):
         resultados.append(
@@ -31,7 +31,7 @@ async def listar_autoridades(distrito_id: int = None, materia_id: int = None, or
 
 @router.get("/{autoridad_id}", response_model=schemas.Autoridad)
 async def consultar_una_autoridad(autoridad_id: int, db: Session = Depends(get_db)):
-    """ Consultar una Autoridad """
+    """Consultar una Autoridad"""
     autoridad = crud.get_autoridad(db, autoridad_id=autoridad_id)
     if autoridad is None:
         raise HTTPException(status_code=400, detail="No existe la autoridad.")
@@ -39,5 +39,7 @@ async def consultar_una_autoridad(autoridad_id: int, db: Session = Depends(get_d
         id=autoridad.id,
         distrito_id=autoridad.distrito_id,
         distrito=autoridad.distrito.nombre,
+        materia_id=autoridad.materia_id,
+        materia=autoridad.materia.nombre,
         autoridad=autoridad.descripcion,
     )
