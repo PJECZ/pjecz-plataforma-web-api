bcdbd1c7f6ed5c49fa91bf498bd8914a73da4c79
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-15 08:19:26 -0500

    Comienza materias.

--- sql/autoridades.session.sql
+++ sql/autoridades.session.sql
@@ -1,24 +1,26 @@
--- @block listar
-SELECT autoridades.id,
-    distritos.nombre,
-    autoridades.descripcion
-FROM distritos
-    INNER JOIN autoridades ON autoridades.distrito = distritos.id
-WHERE autoridades.estatus = 'A'
-ORDER BY distritos.nombre,
-    autoridades.descripcion;
--- @block autoridades y materias
-SELECT distritos.nombre AS distrito,
+-- @block listar autoridades activas
+SELECT distritos.nombre AS departamento,
     autoridades.clave,
     autoridades.descripcion
+FROM distritos
+    INNER JOIN autoridades ON autoridades.distrito_id = distritos.id
+WHERE autoridades.estatus = 'A'
+ORDER BY departamento,
+    descripcion;
+-- @block listar juzgados y materias
+SELECT autoridades.organo_jurisdiccional,
+    materias.nombre AS materia,
+    distritos.nombre AS departamento,
+    autoridades.clave,
+    autoridades.descripcion AS juzgado
 FROM autoridades
     INNER JOIN distritos ON autoridades.distrito_id = distritos.id
+    INNER JOIN materias ON autoridades.materia_id = materias.id
 WHERE distritos.es_distrito_judicial = True
     AND autoridades.es_jurisdiccional = True
     AND autoridades.es_notaria = False
     AND autoridades.estatus = 'A'
-ORDER BY distrito,
-    clave ASC -- @block materias
-SELECT nombre
-FROM materias
-ORDER BY id ASC
+ORDER BY organo_jurisdiccional,
+    materia,
+    departamento,
+    clave ASC
