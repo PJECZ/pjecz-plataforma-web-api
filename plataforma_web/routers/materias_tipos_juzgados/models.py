96751767efb0c85e4b7805fce296551f2e25493e
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-03-08 00:35:49 -0600

    Modelos nuevos

---
+++ plataforma_web/routers/materias_tipos_juzgados/models.py
@@ -0,0 +1,28 @@
+"""
+Materias Tipos de Juicios, modelos
+"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from lib.database import Base
+from lib.universal_mixin import UniversalMixin
+
+
+class MateriaTipoJuicio(Base, UniversalMixin):
+    """MateriaTipoJuicio"""
+
+    # Nombre de la tabla
+    __tablename__ = 'materias_tipos_juicios'
+
+    # Clave primaria
+    id = Column(Integer, primary_key=True)
+
+    # Clave foránea
+    materia_id = Column(Integer, ForeignKey("materias.id"), index=True, nullable=False)
+    materia = relationship("Materia", back_populates="materias_tipos_juzgados")
+
+    # Columnas
+    clave = Column(String(256), unique=True, nullable=False)
+    descripcion = Column(String(256), nullable=False)
+
+    # Hijos
+    repsvm_agresores = relationship("REPSVMAgresor", back_populates="materia_tipo_juzgado", lazy="noload")
