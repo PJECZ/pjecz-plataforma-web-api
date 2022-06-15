c3031432e3b94d5ddd72ac0ea1dd48bb1af5f8ae
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-06-15 08:04:51 -0500

    Tests edictos

--- tests/app.py
+++ tests/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 from tests.blueprints.abogados.views import abogados
 from tests.blueprints.audiencias.views import audiencias
+from tests.blueprints.edictos.views import edictos
 from tests.blueprints.listas_de_acuerdos.views import listas_de_acuerdos
 from tests.blueprints.peritos.views import peritos
 from tests.blueprints.redams.views import redams
@@ -16,6 +17,7 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(abogados)
     app.register_blueprint(audiencias)
+    app.register_blueprint(edictos)
     app.register_blueprint(listas_de_acuerdos)
     app.register_blueprint(peritos)
     app.register_blueprint(redams)
